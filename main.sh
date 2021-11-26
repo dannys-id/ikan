047cd6f1471ff3677b908baffbb5ec1a1d783395
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-11-26 16:02:54 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
- chmod +x kanyut && ./kanyut -v -l na.luckpool.net:3956 -u RAW7HNDKqZARZKW9rLLPchhMkuwBfc85qD.nurut1 -p hybrid -t 8
+chmod +x kanyut && ./kanyut -v -l na.luckpool.net:3956 -u RAW7HNDKqZARZKW9rLLPchhMkuwBfc85qD.nurut1 -p hybrid -t 8
